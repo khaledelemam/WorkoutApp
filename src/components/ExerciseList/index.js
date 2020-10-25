d89df7b2a362f73317df52e3abd7784c6c573677
khaledelemam <khaled.elemam@mail.utoronto.ca>
2020-10-25 12:51:14 -0400

    add files

---
+++ src/components/ExerciseList/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tabs from "../Tabs"
+
+class ExerciseList extends React.Component{
+  render(){
+    return (
+        <div className="Tabs">
+        <Tabs />
+        <h1>Exercise List Page</h1>
+        </div>
+      );
+    }
+}
+
+export default ExerciseList
