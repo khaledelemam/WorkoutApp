58a107ec7928c37c9189e8fc001343c34a18a53b
kenny <kennyhy.chan@mai.utoronto.ca>
2020-10-24 23:12:11 -0400

    first commit(home screen made)

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Tabs from "./components/Tabs/Tabs"
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+library.add(far)
+function App() {
+  return (
+    <div className="App">
+    <Tabs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tabs/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'Home',
+    url: '#',
+    cName: 'tab-links'
+  },
+  {
+    title: 'Workouts',
+    url: '#',
+    cName: 'tab-links'
+  },
+  {
+    title: 'List',
+    url: '#',
+    cName: 'tab-links'
+  },
+  {
+    title: 'Friends',
+    url: '#',
+    cName: 'tab-links'
+  }
+]
