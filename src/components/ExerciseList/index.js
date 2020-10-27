10c35c7799573b22e6396a6a60d521d7d6af6c0f
khaledelemam <khaled.elemam@mail.utoronto.ca>
2020-10-27 15:55:07 -0400

    Changed normal checkboxes to MaterialUI checkboxes

--- src/components/ExerciseList/index.js
+++ src/components/ExerciseList/index.js
@@ -1,84 +1,37 @@
 import React from 'react';
 import Tabs from "../Tabs"
-// import Checkbox from '@material-ui/core/Checkbox';
+import './index.css'
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+
 
 class ExerciseList extends React.Component{
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      chest: false,
-      back: false,
-      arms: false,
-      legs: false,
-      core: false,
-
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
-  }
-
   render(){
+
+  const {checkedA, 
+    checkedB, 
+    handleChange} = this.props;
+
+
     return (
         <div className="Tabs">
         <Tabs />
         <h1>Exercise List Page</h1>
+        <div>
+      <FormControlLabel className='label'
+        control={<Checkbox checked={checkedA} onChange={handleChange} name="checkedA"  />}
+        label="Chest"
+      />
+      <br></br>
+      <FormControlLabel className='label'
+        control={<Checkbox checked={checkedB} onChange={handleChange} name="checkedB" />}
+        label= 'Back'
+      />
+     
+    </div>
 
-        <form>
-        <label>
-        <input
-            name="chest"
-            type="checkbox"
-            checked={this.state.chest}
-            onChange={this.handleInputChange} />
-          Chest
-        </label>
-        <br></br>
-        <label>
-        <input
-            name="back"
-            type="checkbox"
-            checked={this.state.back}
-            onChange={this.handleInputChange} />
-          Back
-        </label>
-        <br></br>
-        <label>
-        <input
-            name="arms"
-            type="checkbox"
-            checked={this.state.arms}
-            onChange={this.handleInputChange} />
-          Arms
-        </label><br></br>
-        <label>
-        <input
-            name="legs"
-            type="checkbox"
-            checked={this.state.legs}
-            onChange={this.handleInputChange} />
-        Legs
-        </label><br></br>
-        <label>
-        <input
-            name="core"
-            type="checkbox"
-            checked={this.state.core}
-            onChange={this.handleInputChange} />
-          Core
-        </label>
-      </form>
-
+      
         </div>
       );
     }
