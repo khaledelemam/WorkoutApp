e0fb1f9ff3bea81b12cfa9f346114715f4abdc64
Tony Shi <tonyuniversity.shi@mail.utoronto.ca>
2020-11-02 04:52:59 -0500

    Made the workouts view look better

--- src/components/Workouts/index.js
+++ src/components/Workouts/index.js
@@ -1,31 +1,60 @@
 import React from 'react';
 import './index.css'
 import Tabs from "../Tabs"
+
 import {makeStyles} from '@material-ui/core/styles'
 import {Grid, Paper} from '@material-ui/core'
+import GridList from '@material-ui/core/GridList'
+import GridListTile from '@material-ui/core/GridListTile'
+import GridListTileBar from '@material-ui/core/GridListTileBar'
+import IconButton from '@material-ui/core/IconButton'
+import InfoIcon from '@material-ui/icons/Info'
 
-
+// Hardcoded values
+import cardio from "../WorkoutPics/cardio.jpg"
+import back from "../WorkoutPics/back.jpg"
+import legs from "../WorkoutPics/legs.jpg"
+import arm from "../WorkoutPics/arm.jpg"
+import abs from "../WorkoutPics/abs.jpg"
+import dumbbell from "../WorkoutPics/dumbbell.jpg"
 
 const workoutNames = ["Cardio", "Back", "Legs", "Arms"]
+
+const workouts = [
+  {
+    img: cardio,
+    title: 'Cardio',
+  },
+  {
+    img: back,
+    title: 'Back',
+  },
+  {
+    img: legs,
+    title: 'Legs',
+  },
+  {
+    img: arm,
+    title: 'Arms',
+  }
+]
   
 const favNames = ["Abs", "Dumbbell"]
 
-// Not sure how to style the grid without hooks, and we're using classes so we can't use hooks
-/* const useStyles = makeStyles((theme) => ({
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center'
+const favWorkouts = [
+  {
+    img: abs,
+    title: 'Abdominals',
+  },
+  {
+    img: dumbbell,
+    title: 'Dumbbell',
   }
-
-})) */
+]
 
 class Workouts extends React.Component {
   state = {
     init: ''
-  }
-  
-  constructor(props) {
-      super(props)
   }
 
 
@@ -34,35 +63,47 @@ class Workouts extends React.Component {
     return (
     <div>
       <Tabs/>
-      <div className = 'header1'>
-         <h2>Custom Workouts</h2>
-      </div>
-      <Grid container spacing={3} justify= "center">
-        <Grid item xs={3}>
-          <Paper>{workoutNames[0]}</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper>{workoutNames[1]}</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper>{workoutNames[2]}</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper>{workoutNames[3]}</Paper>
-        </Grid>
-      </Grid>
+      <div className = 'view'>  
 
-      <div className = 'header2'>
-        <h2>Favourite Workouts</h2>
+        <h2>Custom Workouts</h2>
+ 
+        <GridList className = 'gridList' cellHeight={180} cols={3}>
+        {workouts.map((tile) => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              actionIcon = {
+                <IconButton aria-label={`info about ${tile.title}`} className = 'icon'>
+                    <InfoIcon />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+          ))}
+        </GridList>
+
+        <div className = 'header2'>
+          <h2>Favourite Workouts</h2>
+        </div>
+        
+        <GridList className = 'gridList' cellHeight={180} cols={3}>
+        {favWorkouts.map((tile) => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              actionIcon = {
+                <IconButton aria-label={`info about ${tile.title}`} className = 'icon'>
+                    <InfoIcon />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+          ))}
+        </GridList>
+
       </div>
-      <Grid container spacing={3} justify= "center">
-        <Grid item xs={6}>
-          <Paper>{favNames[0]}</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper>{favNames[1]}</Paper>
-        </Grid>
-      </Grid>
     </div>
     ) 
   }
