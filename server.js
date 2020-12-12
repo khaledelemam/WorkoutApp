3d9df0bff08f399297df45bf4e745b232e02e5b5
Khaled Elemam <khaledemx@gmail.com>
2020-12-11 19:18:29 -0500

    more eoutes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/* server.js for react-express-authentication */
+
 "use strict";
 
 /* Server environment setup */
@@ -28,7 +28,7 @@ const { mongoose } = require("./db/mongoose");
 mongoose.set('useFindAndModify', false); // for some deprecation issues
 
 // import the mongoose models
-const { User } = require("./models/users");
+const { User } = require("./models/user");
 
 // to validate object IDs
 const { ObjectID } = require("mongodb");
@@ -97,6 +97,57 @@ app.use(
         store: env === 'production' ? new MongoStore({ mongooseConnection: mongoose.connection }) : null
     })
 );
+
+
+// A route to login and create a session
+app.post("/users/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // log(email, password);
+    // Use the static method on the User model to find a user
+    // by their email and password
+    User.findByEmailPassword(email, password)
+        .then(user => {
+            // Add the user's id to the session.
+            // We can check later if this exists to ensure we are logged in.
+            req.session.user = user._id;
+            req.session.email = user.email; // we will later send the email to the browser when checking if someone is logged in through GET /check-session (we will display it on the frontend dashboard. You could however also just send a boolean flag).
+            res.send({ currentUser: user.email });
+        })
+        .catch(error => {
+            res.status(400).send()
+        });
+});
+
+// A route to logout a user
+app.get("/users/logout", (req, res) => {
+    // Remove the session
+    req.session.destroy(error => {
+        if (error) {
+            res.status(500).send(error);
+        } else {
+            res.send()
+        }
+    });
+});
+
+// A route to check if a user is logged in on the session
+app.get("/users/check-session", (req, res) => {
+    if (env !== 'production' && USE_TEST_USER) { // test user on development environment.
+        req.session.user = TEST_USER_ID;
+        req.session.email = TEST_USER_EMAIL;
+        res.send({ currentUser: TEST_USER_EMAIL })
+        return;
+    }
+
+    if (req.session.user) {
+        res.send({ currentUser: req.session.email });
+    } else {
+        res.status(401).send();
+    }
+});
+
 
 /*** Webpage routes below **********************************/
 // Serve the build
